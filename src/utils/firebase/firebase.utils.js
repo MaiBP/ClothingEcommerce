4bd5174b382a1189888884b2f188bf2761e7d921
Maira Pajaro <maira_pajaro@hotmail.com>
2022-08-03 17:13:25 +0200

    data added to firebase and shop preview component created

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -11,7 +11,15 @@ import {
 
 } from 'firebase/auth'
 
-import { getFirestore, doc, getDoc, setDoc} from 'firebase/firestore'
+import { 
+    getFirestore, 
+    doc, 
+    getDoc, 
+    setDoc, 
+    collection, 
+    writeBatch, 
+    query, 
+    getDocs} from 'firebase/firestore'
 
 
 const firebaseConfig = {
@@ -37,6 +45,31 @@ export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider)
 // export const signInWithGoogleRedirect = () => signInWithRedirect(auth,googleProvider)
 
 export const db = getFirestore();
+
+export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) =>{
+    const collectionRef = collection(db, collectionKey);
+    const batch = writeBatch(db);
+
+    objectsToAdd.forEach((object) =>{
+        const docRef = doc(collectionRef, object.title.toLowerCase());
+        batch.set(docRef, object);
+    })
+    await batch.commit();
+    console.log('done')
+}
+
+// Get the data info from firebase
+export const getCategoriesAndDocuments = async () => {
+    const collectionRef = collection(db, 'categories')
+    const q = query(collectionRef);
+    const querySnapshot = await getDocs(q);
+    const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot)=>{
+        const {title, items} = docSnapshot.data();
+        acc[title.toLowerCase()] = items;
+        return acc;
+    },{})
+    return categoryMap;
+}
 
 export const createUserDocumentFromAuth = async (userAuth, additionalInformation={}) =>{
     if (!userAuth) return;
